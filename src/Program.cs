c4e7227aa12db26a4a4ac8706708eb6d006ec4ad
Pp-4 <39858427+Pp-4@users.noreply.github.com>
2025-08-07 16:42:41 +0200

    reorganized file structore for ssg, added c# project files

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+namespace sitebuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello, World!");
+            Debug.WriteLine("asdad");
+        }
+    }
+}
